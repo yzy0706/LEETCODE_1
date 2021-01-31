51c6ee1d9b9f87005dfe69bb8bdb3676fa06085f
Zhongyu <yizhongyu2014@gmail.com>
2021-01-30 17:33:06 -0800

    stack can be finished tmr

--- src/Stack/shuntingYard/LargestRectangleArea.java
+++ src/Stack/shuntingYard/LargestRectangleArea.java
@@ -16,6 +16,8 @@ public class LargestRectangleArea {
                 int highestIndex = stack.pop();
                 res = Math.max(res, heights[highestIndex] * (stack.isEmpty()? i : i - 1 - stack.peek()));
                 i--;
+                //比如说i是1的时候， i--到0， 下一轮开始前又会++到1， 这样下一轮开始以后因为heights[0]已经被pop出去了，
+                // 所以会把height[1]加进去当作左边最高的高度
             }
         }
         return res;
